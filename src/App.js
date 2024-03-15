b79129ffabdacbd22ef7e12babd8487fd2bd67ad
santibrizuela <santibrizuela.97@gmail.com>
2024-03-15 00:02:06 -0300

    footer link

--- src/App.js
+++ src/App.js
@@ -8,6 +8,7 @@ import { Routes, Route} from 'react-router-dom';
 import Home from './pages/Home';
 import Catalogo from './pages/Catalogo';
 import Navbar from './components/Navbar';
+import Footer from './components/Footer';
 
 const App = () => {
   return (
@@ -22,6 +23,7 @@ const App = () => {
         <Route path='/catalogo' element={<Catalogo/>} />
       </Routes>
       <Contact />
+      <Footer />
     </div>
   );
 };
---
+++ src/components/Footer.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Footer = () => {
+  return (
+    <div id='footer' className='w-full text-center text-gray-300'>
+        <p className='bg-blue-900 mt-2 py-4 text-sm'>
+          <a href='https://brizuelasideravicius.vercel.app/' target="_blank" rel="noopener noreferrer">Brizuela Sideravicius</a> © Todos los derechos reservados | Diseño y Desarrollo Web.
+        </p>
+    </div>
+  )
+}
+
+export default Footer
